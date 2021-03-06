18a1598741a0f398d02d2087673bb82ea3490106
Priyanshu Bhardwaj <63417899+priyanshu-beep@users.noreply.github.com>
2021-03-06 16:51:41 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,26 +2,25 @@ vector<pair<int, int> > g[N];
 const int inf = 1e9 + 10;
 vi dist(N, inf);
 
-void shortestPath(int src, int n)
+void dijkstra(int source)
 {
 
-    priority_queue< pii, vector <pii> , greater<pii> > pq;
+    dist[source] = 0;
+    set<pair<int, int> > s;
+    s.insert({0, source});
 
-    pq.push(make_pair(0, src));
-    dist[src] = 0;
+    while (!s.empty())
+    {
+        auto x = *(s.begin());
+        s.erase(x);
+        for (auto it : g[x.second])
+            if (dist[it.first] > dist[x.second] + it.second)
+            {
+                s.erase({dist[it.first], it.first});
+                dist[it.first] = dist[x.second] + it.second;
+                s.insert({dist[it.first], it.first});
 
-    vector<bool> f(n, false);
-    while (!pq.empty()) {
-        int u = pq.top().S;
-        pq.pop();
-        f[u] = true;
-
-        for (auto it : g[u])
-            if (f[it.F] == false && dist[it.F] > dist[u] + it.S) {
-                dist[it.F] = dist[u] + it.S;
-                pq.push(make_pair(dist[it.F], it.F));
+                par[it.F] = x.S;
             }
     }
-
 }
-
