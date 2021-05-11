091fd794dd136940e1ce8b6820018b40b3430cfc
Priyanshu Bhardwaj <63417899+priyanshu-beep@users.noreply.github.com>
2021-05-11 11:04:33 +0530

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,74 +1,18 @@
-https://codeforces.com/contest/242/submission/112087943
-int n;
-int t[4 * N];
 
 
-void build(int a[], int v, int tl, int tr) {
-    if (tl == tr) {
-        t[v] = a[tl];
-
-
-        return;
-    }
-
-
-    int tm = (tl + tr) / 2;
-
-    build(a, v * 2, tl, tm);
-    build(a, v * 2 + 1, tm + 1, tr);
-
-
-    t[v] = t[v * 2] + t[v * 2 + 1];
-
-}
-
-int querry(int v, int tl, int tr, int l, int r) {
-    if (l > r)
-        return 0;
-
-    if (l == tl && r == tr) {
-        return t[v];
-
-    }
-    int tm = (tl + tr) / 2;
-
-    return querry(v * 2, tl, tm, l, min(r, tm)) + querry(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
-}
-
-
-void update(int v, int tl, int tr, int pos, int new_val) {
-    
-    if (tl == tr) {
-        t[v] = new_val;
-
-        return;
-    }
-
-    int tm = (tl + tr) / 2;
-    if (pos <= tm)
-        update(v * 2, tl, tm, pos, new_val);
-    else
-        update(v * 2 + 1, tm + 1, tr, pos, new_val);
-
-
-    t[v] = t[v * 2] + t[v * 2 + 1];
-
-}
-
-
-struct data
+struct Data
 {
     //Use required attributes
     ll mn;
 
     //Default Values
-    data() : mn(0) {};
+    Data() : mn(0) {};
 };
 
 struct SegTree
 {
     int N;
-    vector<data> st;
+    vector<Data> st;
     vector<bool> cLazy;
     vector<int> lazy;
 
@@ -81,7 +25,7 @@ struct SegTree
     }
 
     //Write reqd merge functions
-    void merge(data &cur, data &l, data &r)
+    void merge(Data &cur, Data &l, Data &r)
     {
         cur.mn = l.mn + r.mn;
     }
@@ -114,23 +58,23 @@ struct SegTree
         merge(st[node], st[node * 2], st[node * 2 + 1]);
     }
 
-    data Query(int node, int L, int R, int i, int j)
+    Data Query(int node, int L, int R, int i, int j)
     {
         if (cLazy[node])
             propagate(node, L, R);
         if (j < L || i > R)
-            return data();
+            return Data();
         if (i <= L && R <= j)
             return st[node];
         int mid = (L + R) / 2;
-        data left = Query(node * 2, L, mid, i, j);
-        data right = Query(node * 2 + 1, mid + 1, R, i, j);
-        data cur;
+        Data left = Query(node * 2, L, mid, i, j);
+        Data right = Query(node * 2 + 1, mid + 1, R, i, j);
+        Data cur;
         merge(cur, left, right);
         return cur;
     }
 
-    data pQuery(int node, int L, int R, int pos)
+    Data pQuery(int node, int L, int R, int pos)
     {
         if (cLazy[node])
             propagate(node, L, R);
@@ -183,12 +127,12 @@ struct SegTree
         merge(st[node], st[node * 2], st[node * 2 + 1]);
     }
 
-    data query(int pos)
+    Data query(int pos)
     {
         return pQuery(1, 1, N, pos);
     }
 
-    data query(int l, int r)
+    Data query(int l, int r)
     {
         return Query(1, 1, N, l, r);
     }
@@ -207,4 +151,3 @@ struct SegTree
         Build(1, 1, N, ar);
     }
 };
-
