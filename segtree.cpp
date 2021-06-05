37dc1c94646d05aea41a69c6adcc1ae8ae49246c
Priyanshu Bhardwaj <63417899+priyanshu-beep@users.noreply.github.com>
2021-06-05 22:37:08 +0530

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -151,3 +151,101 @@ struct SegTree
         Build(1, 1, N, ar);
     }
 };
+
+
+
+
+
+
+// for point update only no lazy propagation
+
+
+struct Data
+{
+    //Use required attributes
+    ll mn;
+
+    //Default Values
+    Data() : mn(0) {};
+};
+
+struct SegTree
+{
+    int N;
+    vector<Data> st;
+
+    void init(int n)
+    {
+        N = n;
+        st.resize(4 * N + 5);
+    }
+
+    //Write reqd merge functions
+    void merge(Data &cur, Data &l, Data &r)
+    {
+        cur.mn = l.mn + r.mn;
+    }
+
+    void Build(int node, int L, int R, int ar[])
+    {
+        if (L == R)
+        {
+            st[node].mn = ar[L];
+            return;
+        }
+        int mid = (L + R) / 2;
+        Build(node * 2, L, mid, ar);
+        Build(node * 2 + 1, mid + 1, R, ar);
+        merge(st[node], st[node * 2], st[node * 2 + 1]);
+    }
+
+
+    Data Query(int node, int L, int R, int i, int j) {
+
+        if (j < L || i > R)
+            return Data();
+        if (i <= L && R <= j)
+            return st[node];
+        
+        int mid = (L + R) / 2;
+        Data left = Query(node * 2, L, mid, i, j);
+        Data right = Query(node * 2 + 1, mid + 1, R, i, j);
+        Data cur;
+        merge(cur, left, right);
+        return cur;
+    }
+
+    void pUpdate(int node, int L, int R, int pos, int val)
+    {
+        if (L == R) {
+            st[node].mn = val;
+            return;
+        }
+
+        int mid = (L + R) / 2;
+        if (pos <= mid)
+            pUpdate(node * 2, L, mid, pos, val);
+        else
+            pUpdate(node * 2 + 1, mid + 1, R, pos, val);
+
+        merge(st[node], st[node * 2], st[node * 2 + 1]);
+    }
+
+
+
+    Data query(int l, int r)
+    {
+        return Query(1, 1, N, l, r);
+    }
+
+    void update(int pos, int val)
+    {
+        pUpdate(1, 1, N, pos, val);
+    }
+
+    void build(int ar[]) {
+        Build(1, 1, N, ar);
+    }
+};
+
+
